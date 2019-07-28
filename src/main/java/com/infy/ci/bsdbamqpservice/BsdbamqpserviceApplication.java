a9a4832632fbccde7f7343ff649ceec169b3fa1f
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-28 18:59:22 +0530

    code

---
+++ src/main/java/com/infy/ci/bsdbamqpservice/BsdbamqpserviceApplication.java
@@ -0,0 +1,13 @@
+package com.infy.ci.bsdbamqpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsdbamqpserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsdbamqpserviceApplication.class, args);
+	}
+
+}
