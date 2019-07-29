7f36e0d8aff0b26d1adefaf320c75ce3fd1755b8
Sayed <Irfan.Sayed@Honeywell.com>
2019-07-29 12:42:14 +0530

    code

---
+++ src/main/java/com/infy/ci/bsdbamqpservice/AppConfig.java
@@ -0,0 +1,10 @@
+package com.infy.ci.bsdbamqpservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = { "com.infy.ci.bsdbamqpservice" })
+public class AppConfig {
+
+}
